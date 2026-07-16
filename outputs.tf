d8e3c051143f1bb4f08cbad7d8a460624233ba5d
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:50:58 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "site_recovery_replication_policies_id" {
   description = "Map of id values across all site_recovery_replication_policies, keyed the same as var.site_recovery_replication_policies"
-  value       = { for k, v in azurerm_site_recovery_replication_policy.site_recovery_replication_policies : k => v.id }
+  value       = { for k, v in azurerm_site_recovery_replication_policy.site_recovery_replication_policies : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "site_recovery_replication_policies_application_consistent_snapshot_frequency_in_minutes" {
   description = "Map of application_consistent_snapshot_frequency_in_minutes values across all site_recovery_replication_policies, keyed the same as var.site_recovery_replication_policies"
-  value       = { for k, v in azurerm_site_recovery_replication_policy.site_recovery_replication_policies : k => v.application_consistent_snapshot_frequency_in_minutes }
+  value       = { for k, v in azurerm_site_recovery_replication_policy.site_recovery_replication_policies : k => v.application_consistent_snapshot_frequency_in_minutes if v.application_consistent_snapshot_frequency_in_minutes != null }
 }
 output "site_recovery_replication_policies_name" {
   description = "Map of name values across all site_recovery_replication_policies, keyed the same as var.site_recovery_replication_policies"
-  value       = { for k, v in azurerm_site_recovery_replication_policy.site_recovery_replication_policies : k => v.name }
+  value       = { for k, v in azurerm_site_recovery_replication_policy.site_recovery_replication_policies : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "site_recovery_replication_policies_recovery_point_retention_in_minutes" {
   description = "Map of recovery_point_retention_in_minutes values across all site_recovery_replication_policies, keyed the same as var.site_recovery_replication_policies"
-  value       = { for k, v in azurerm_site_recovery_replication_policy.site_recovery_replication_policies : k => v.recovery_point_retention_in_minutes }
+  value       = { for k, v in azurerm_site_recovery_replication_policy.site_recovery_replication_policies : k => v.recovery_point_retention_in_minutes if v.recovery_point_retention_in_minutes != null }
 }
 output "site_recovery_replication_policies_recovery_vault_name" {
   description = "Map of recovery_vault_name values across all site_recovery_replication_policies, keyed the same as var.site_recovery_replication_policies"
-  value       = { for k, v in azurerm_site_recovery_replication_policy.site_recovery_replication_policies : k => v.recovery_vault_name }
+  value       = { for k, v in azurerm_site_recovery_replication_policy.site_recovery_replication_policies : k => v.recovery_vault_name if v.recovery_vault_name != null && length(v.recovery_vault_name) > 0 }
 }
 output "site_recovery_replication_policies_resource_group_name" {
   description = "Map of resource_group_name values across all site_recovery_replication_policies, keyed the same as var.site_recovery_replication_policies"
-  value       = { for k, v in azurerm_site_recovery_replication_policy.site_recovery_replication_policies : k => v.resource_group_name }
+  value       = { for k, v in azurerm_site_recovery_replication_policy.site_recovery_replication_policies : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 
